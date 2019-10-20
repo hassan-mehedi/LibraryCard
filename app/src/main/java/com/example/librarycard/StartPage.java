6c7b460c7cd97088e1eb1965a18c8f265df2c2a7
Mehedi222 <mehedihassan2420@gmail.com>
2019-10-20 22:09:46 +0600

    Just for saving work progress.

---
+++ app/src/main/java/com/example/librarycard/StartPage.java
@@ -0,0 +1,14 @@
+package com.example.librarycard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
